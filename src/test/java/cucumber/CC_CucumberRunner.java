2c97db63426704313abae9282f6d01c939b3cb99
Adriano Tanoue <adriano.katayamatanoue@asml.com>
2020-05-04 17:50:49 +0200

    cucumber runner

---
+++ src/test/java/cucumber/CC_CucumberRunner.java
@@ -0,0 +1,11 @@
+package cucumber;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty" }, features = { "src/test/resources/features" })
+public class CC_CucumberRunner {
+}
