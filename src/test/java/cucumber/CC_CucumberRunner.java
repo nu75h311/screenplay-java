22d797b0a4c35e389e36da89c147bbe0bf3bfb23
Adriano Tanoue <adriano.tanoue@gmail.com>
2020-06-08 21:57:55 +0200

    added a remote cucumber report

--- src/test/java/cucumber/CC_CucumberRunner.java
+++ src/test/java/cucumber/CC_CucumberRunner.java
@@ -6,8 +6,10 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/resources/features" },
-                 plugin = { "pretty", "html:target/cucumber/report.html" },
-                 strict = true)
+@CucumberOptions(features = {"src/test/resources/features"},
+        plugin = {"progress", "html:target/cucumber/report.html",
+                "message:https://b1f2yk58d8.execute-api.eu-west-3.amazonaws.com/api/repository/232ee238-cddf-4f94-ad90-52ab98b3ab43"},
+        strict = true)
 public class CC_CucumberRunner {
+
 }
