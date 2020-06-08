d559321596b8299b1d41cc0ba2d6d7c4b13809fd
Adriano Tanoue <adriano.tanoue@gmail.com>
2020-06-08 21:01:12 +0200

    now using WebDriverManager

--- src/main/java/utils/WebdriverInitializer.java
+++ src/main/java/utils/WebdriverInitializer.java
@@ -3,10 +3,7 @@ package utils;
 import static org.openqa.selenium.remote.BrowserType.CHROME;
 import static org.openqa.selenium.remote.BrowserType.FIREFOX;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
@@ -17,36 +14,19 @@ public class WebdriverInitializer {
     static Logger logger = LogManager.getLogger("WebdriverInitializer");
 
     public static WebDriver getDriverFor(String browserType) {
-        loadConfigFile();
         WebDriver driver;
         switch (browserType) {
             case CHROME:
+                WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
             case FIREFOX:
             default:
+                WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 break;
         }
         driver.manage().window().maximize();
         return driver;
-    }
-
-    private static void loadConfigFile() {
-        try (InputStream propFile = WebdriverInitializer.class.getClassLoader().getResourceAsStream("config.properties")) {
-
-            Properties props = new Properties();
-
-            if (propFile == null) {
-                logger.warn("Sorry, unable to find config.properties");
-                return;
-            }
-
-            props.load(propFile);
-            props.forEach((key, value) -> System.setProperty(key.toString(), value.toString()));
-
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
     }
 }
